45ae7b7937221e91583c730ca1ecc69405caa302
Anni <anni.a.virtanen@tuni.fi>
2026-01-10 15:57:59 +0200

    initialize template

---
+++ teletext-app/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js with React.js and TypeScript!</h1>
+    </div>
+  );
+};
+
+export default Home;
